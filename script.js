928e27e86b4d9f12d81f20d3433838542c496199
zalimmuslimbadshah-lab <zalimmuslimbadshah@gmail.com>
2026-08-02 06:05:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,41 +7,29 @@ const form = document.getElementById("meetForm");
 const message = document.getElementById("message");
 const button = document.getElementById("submitBtn");
 
-form.addEventListener("submit", function(e){
-
+form.addEventListener("submit", function(e) {
     e.preventDefault();
 
     button.disabled = true;
     button.innerHTML = "Submitting...";
 
     emailjs.sendForm(
-        "service_q1swjs",
-        "template_16alkqj",
+        "service_qj1swjs",
+        "template_om21cd8",
         this
     )
-
-    .then(function(){
-
+    .then(function() {
         message.style.color = "#00ff99";
         message.innerHTML = "✅ Registration Submitted Successfully!";
-
         form.reset();
-
         button.disabled = false;
         button.innerHTML = "Submit Registration";
-
     })
-
-    .catch(function(error){
-
+    .catch(function(error) {
         console.log(error);
-
         message.style.color = "#ffff66";
         message.innerHTML = "❌ Failed! Please try again.";
-
         button.disabled = false;
         button.innerHTML = "Submit Registration";
-
     });
-
 });
